796aefb6065dd8b6cf6a79e1b60397899e485d72
Nicolau FS <nicolau.farre@gmail.com>
2024-06-03 20:20:43 +0700

    added security headers

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,41 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        // Aplicar estas cabeceras a todas las rutas
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; img-src 'self' https: data:; media-src 'self'",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "no-referrer",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withNextIntl(nextConfig);
