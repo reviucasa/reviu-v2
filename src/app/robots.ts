53bd0b6468816857d17fd3f01169e3c450a006a2
Nicolau FS <nicolau.farre@gmail.com>
2024-06-13 13:43:34 +0200

    updated url in sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/admin/",
     },
-    sitemap: "https://reviu.vercel.app/sitemap.xml",
+    sitemap: "https://reviucasa.com/sitemap.xml",
   };
 }
