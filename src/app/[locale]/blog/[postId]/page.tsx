c01b785d30a1469858f00fec548b0f68f28e351a
Nicolau FS <nicolau.farre@gmail.com>
2024-07-02 23:29:52 +0200

    Update page.tsx

--- src/app/[locale]/blog/[postId]/page.tsx
+++ src/app/[locale]/blog/[postId]/page.tsx
@@ -24,10 +24,10 @@ export async function generateMetadata({
 
   const description =
     locale == "en"
-      ? `${p?.title} - Read more on Reviu.`
+      ? `${p?.subtitle} - Read more on Reviu.`
       : locale == "es"
-      ? `${p?.title} - Lee más en Reviu.`
-      : `${p?.title} - Llegeix més a Reviu.`;
+      ? `${p?.subtitle} - Lee más en Reviu.`
+      : `${p?.subtitle} - Llegeix més a Reviu.`;
 
   return {
     title: titleDetail,
