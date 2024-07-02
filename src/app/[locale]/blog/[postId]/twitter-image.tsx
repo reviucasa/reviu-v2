533de1dbb98e893fc7279b7dcdab6712fc51ea07
Nicolau FS <nicolau.farre@gmail.com>
2024-07-02 18:57:37 +0200

    trying twitter  image for post

---
+++ src/app/[locale]/blog/[postId]/twitter-image.tsx
@@ -0,0 +1,62 @@
+import { getPost } from "@/models/post";
+import { ImageResponse } from "next/og";
+
+// Route segment config
+export const runtime = "edge";
+
+// Image metadata
+export const alt = "About Acme";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+// Image generation
+export default async function Image({
+  params: { postId },
+}: {
+  params: { locale: string; postId: string };
+}) {
+  // Fetch blog post data using the postId
+  const post = await getPost(postId);
+
+  // Font
+  const ppeUltrabold = fetch(
+    new URL("./fonts/PPEditorialNew-Ultrabold.otf", import.meta.url)
+  ).then((res) => res.arrayBuffer());
+
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 128,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {post?.imageUrl}
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+      fonts: [
+        {
+          name: "PPEditorialNew",
+          data: await ppeUltrabold,
+          style: "normal",
+          weight: 700,
+        },
+      ],
+    }
+  );
+}
