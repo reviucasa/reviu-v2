f19db00e281b6cb784843b479a5cba7226c4ff35
Nicolau FS <nicolau.farre@gmail.com>
2024-09-19 12:53:32 +0200

    new version for opengraph, lets see

--- src/app/[locale]/blog/[postId]/page.tsx
+++ src/app/[locale]/blog/[postId]/page.tsx
@@ -6,6 +6,14 @@ import { getPost } from "@/models/post";
 import { mainKeywords } from "@/staticData";
 import { notFound } from "next/navigation";
 
+import Logo from "public/images/reviuLogo.svg";
+
+import { Space_Grotesk } from "next/font/google";
+import NextImage from "next/image";
+
+// Load Space Grotesk font
+const space_grotesk = Space_Grotesk({ subsets: ["latin"] });
+
 export async function generateMetadata({
   params: { locale, postId },
 }: {
@@ -37,7 +45,7 @@ export async function generateMetadata({
     description,
     keywords,
     metadataBase: new URL(`https://www.reviucasa.com/${locale}/blog/${postId}`),
-    openGraph: {
+    /* openGraph: {
       type: "article",
       url: `https://www.reviucasa.com/${locale}/blog/${postId}`,
       title: p?.title,
@@ -53,7 +61,7 @@ export async function generateMetadata({
       title: p?.title,
       description: p?.subtitle,
       image: post?.imageUrl,
-    },
+    }, */
   };
 }
 
@@ -70,9 +78,48 @@ export default async function PostPage({
     notFound();
   }
 
+  const imageUrl = post?.imageUrl;
+  const p = post?.translations[params.locale as "en" | "ca" | "es"];
+
   return (
     <MainLayout>
       <div className="max-w-5xl mx-auto my-20 rounded-2xl p-8 bg-white">
+        <div className="flex items-center justify-center w-full h-full bg-white p-6">
+          {/* Card Container */}
+          <div className="bg-white shadow-lg rounded-lg overflow-hidden flex flex-col items-center w-full h-full">
+            {/* Post Image */}
+            <div className="relative w-full h-2/3">
+              <NextImage
+                src={imageUrl}
+                alt={p?.title ?? "Post image"}
+                layout="fill"
+                objectFit="cover"
+                className="w-full h-full object-cover"
+              />
+            </div>
+
+            {/* Card Content */}
+            <div className="p-4 flex flex-col items-start w-full h-1/3">
+              {/* Post Title */}
+              <h1
+                className={`${space_grotesk.className} text-black text-3xl font-bold mb-2`}
+              >
+                {p?.title}
+              </h1>
+
+              {/* Reviucasa Logo */}
+              <div className="w-24 mt-4">
+                <NextImage
+                  src={Logo}
+                  alt="Reviucasa Logo"
+                  width={100}
+                  height={40}
+                  className="object-contain"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
         <PostView
           post={{
             ...post,
