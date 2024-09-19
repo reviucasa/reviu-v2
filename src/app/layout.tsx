5edab5f0eb51910956e88e62e35f798fee094375
Nicolau FS <nicolau.farre@gmail.com>
2024-09-20 01:18:39 +0200

    trying just with code and public images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,68 @@
+import { mainKeywords } from "@/staticData";
 import { ReactNode } from "react";
 
 type Props = {
   children: ReactNode;
 };
+
+export async function generateMetadata({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  const titleDetail =
+    locale == "en"
+      ? "Home reviews you can trust"
+      : locale == "es"
+      ? "Reseñas de pisos reales"
+      : "Ressenyes de pisos reals";
+
+  const description =
+    locale == "en"
+      ? "Share and find anonymous reviews about homes in your city. Access experiences of those who have already lived in the home you are looking for and share your own experiences about the places you have lived."
+      : locale == "es"
+      ? "Comparte y encuentra reseñas y opiniones anónimas sobre pisos de tu ciudad. Infórmate de cómo es vivir en la casa que te interesa antes de mudarte y comparte opiniones sobre los lugares donde has vivido."
+      : "Comparteix i troba ressenyes i opinions anònimes sobre pisos de la teva ciutat. Informa't de com és viure a la casa que t'interessa abans de mudar-t'hi i comparteix opinions sobre els llocs on has viscut.";
+
+  const keywords = mainKeywords(locale);
+
+  return {
+    metadataBase: new URL(`https://www.reviucasa.com`),
+    title: "Reviu | " + titleDetail,
+    description,
+    keywords,
+    openGraph: {
+      title: titleDetail,
+      description,
+      url: new URL(`https://www.reviucasa.com`),
+      siteName: "Reviu",
+      locale: locale,
+      type: "website",
+      images: [
+        {
+          url: "https://www.reviucasa.com/images/home-opengraph-image.png",
+        },
+      ],
+    },
+    robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+        index: true,
+        follow: true,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+      },
+    },
+    twitter: {
+      title: titleDetail,
+      description,
+      card: "summary_large_image",
+      images: ["https://www.reviucasa.com/images/home-opengraph-image.png"],
+    },
+  };
+}
 
 // Since we have a `not-found.tsx` page on the root, a layout file
 // is required, even if it's just passing children through.
